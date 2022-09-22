466067528a1ba7e0a679f27bfcf5dd9e15c890c4
briotisk <briotisk@gmail.com>
2022-09-22 17:37:54 +0000

    Entrada de palavras e impressão simultânea para o modo Dueto finalizado

--- termo.asm
+++ termo.asm
@@ -472,7 +472,7 @@ ImprimeLetraTermo:
 	push r1
 	push r2
 
-	outchar r2, r0			;imprime o caractere de r2 na posição de r0
+	outchar r2, r0 ;imprime o caractere de r2 na posição de r0
 
 	pop r2
 	pop r1
@@ -487,7 +487,12 @@ ImprimeLetraDueto:
 	push r0
 	push r1
 	push r2
+	
+	loadn r1, #15 ;espaço entre as palavras impressas somado ao tamanho das mensagens
 
+	outchar r2, r0 ;imprime o caractere de r2 na posição de r0
+	add r0, r0, r1 ;seta a posição para imprimir a nova mensagem com 10 pixels de distancia da primeira
+	outchar r2, r0 ;imprime o caractere de r2 na posição de r0
 
 	pop r2
 	pop r1
@@ -545,7 +550,7 @@ Termo:
 	pop r0
 	pop fr
 
-	breakp;implementar
+	breakp
 	rts
 ;-----------------------------------------------
 
@@ -602,8 +607,35 @@ DesenhaTelaTermo:
 ;-----------------------------------------------
 Dueto:
 	
+		push fr
+	push r0
+	push r1
+	push r2
+	push r3
+	push r4
+	
 	Call DesenhaTelaDueto
-	breakp;implementar
+
+	loadn r0, #369 ;carrega a posição na qual deve se iniciar a impressão
+	loadn r1, #80  ;fator para pular duas linhas
+	loadn r2, #7   ;numero de tentativas
+
+	Dueto_Loop:
+	
+		Call InputPalavra ;lê a palavra
+		add r0, r0, r1	  ;seta a posição para imprimir a nova mensagem duas linhas abaixo
+		;Call Compara;falta implementar
+		dec r2;
+		jnz Dueto_Loop
+
+	pop r4
+	pop r3
+	pop r2
+	pop r1
+	pop r0
+	pop fr
+
+	breakp
 	rts
 ;-----------------------------------------------
 
